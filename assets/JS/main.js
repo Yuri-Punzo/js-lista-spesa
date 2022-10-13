c74b03b796ae2ce702a2fcb789f218f133fc8c43
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-13 16:01:36 +0200

    aggiunta terza soluzione con innerHTML +=

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -18,6 +18,16 @@ while (i < shoppingList.length) {
     const liElement = document.createElement("li")
     ulSelection.append(liElement)
     liElement.append(listItems)
+
     /* VERSIONE BRUTTA ma più sintetica */
+
     /* document.getElementById("lista_spesa").insertAdjacentHTML("beforeend", `${listItems} `) */
+
+    /* ALTRA VERSIONE con innerHTML */
+
+    /* ulSelection.innerHTML += `<li>${listItems}</li>` */
+
+    /* questa versione FUNZIONA SOLO con += altrimenti scrive solo l'ultimo elemento della lista invece con l'assegnazione += li scrive tutti.
+    
+    The addition assignment operator (+=) adds the value of the right operand to a variable and assigns the result to the variable. The types of the two operands determine the behavior of the addition assignment operator. Addition or concatenation is possible. */
 }
